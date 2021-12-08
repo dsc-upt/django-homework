a7bd9063e992dedd53ce29b3242fc262e0916b84
N-Octavian <n.octavian92@gmail.com>
2021-12-08 09:11:35 +0200

    removed extra stuff

--- src/backend/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Article(models.Model):
-    title = models.CharField(max_length=120)
-    content = models.TextField(blank = False, null = False)
-    date = models.DateField(auto_now_add=True)
-
-
