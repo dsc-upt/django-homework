d953ed780137d9f659d2a6f34d46c8a4ddb65e41
VladS159 <sarbuvlad6@yahoo.com>
2021-12-18 13:20:18 +0200

    removed files.

--- articles_django/articles/backend/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Article(models.Model):
-    title = models.CharField(max_length = 64)
-    content = models.TextField()
-    date = models.DateTimeField(auto_now_add = True)
